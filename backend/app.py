469a5663b40737cadb187c0e2830700897cfd543
Snehapattar <snehap226704@gmail.com>
2026-03-12 14:32:02 +0530

    updated

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+import os
+
 from routes.sensor_routes import sensor_bp
 from routes.auth_routes import auth_bp
 from routes.reward_routes import admin_bp, reward_bp
 from routes.global_routes import global_bp
 
 app = Flask(__name__)
+
 CORS(app)
 
 app.register_blueprint(sensor_bp, url_prefix="/api")
@@ -14,5 +17,12 @@ app.register_blueprint(reward_bp, url_prefix="/api/rewards")
 app.register_blueprint(admin_bp, url_prefix="/api/admin")
 app.register_blueprint(global_bp, url_prefix="/api")
 
+
+@app.route("/")
+def home():
+    return {"message": "GreenPulse Backend Running!"}
+
+
+# Run server
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
